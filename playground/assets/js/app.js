750253e7cd65a8c8fef370488b687cc456762355
Chad Hartman <cmhartman+github@gmail.com>
2020-10-06 13:45:14 -0700

    Refining playground

--- playground/assets/js/app.js
+++ playground/assets/js/app.js
@@ -19,58 +19,6 @@
         .then(buffer => WebAssembly.instantiate(buffer))
         .then(wasm => wasm.instance.exports);
 
-    function yFormatter(label) {
-        if (label < 1000) {
-            return label;
-        }
-
-        if (label < 1000000) {
-            label / 1000 + "K"
-        }
-
-        return label < 1000000000 ?
-            label / 1000000 + "M" :
-            label / 1000000000 + "B";
-    };
-
-    function chart(ctx, rng) {
-
-        const total = 100;
-        let data = new Array(total);
-        let labels = new Array(total);
-        for (let i = 0; i < total; ++i) {
-            data[i] = rng();
-            labels[i] = i;
-        }
-
-        new Chart(ctx, {
-            type: "line",
-            data: {
-                labels: labels,
-                datasets: [
-                    {
-                        data: data,
-                        fill: false,
-                        borderColor: "#36e"
-                    }
-                ]
-            },
-            options: {
-                legend: {
-                    display: false
-                },
-                responsive: true,
-                scales: {
-                    yAxes: [
-                        {
-                            ticks: { callback: yFormatter }
-                        }
-                    ]
-                }
-            }
-        });
-    };
-
     app.vue = new Vue({
         el: '#vueapp',
         data: {
@@ -80,22 +28,39 @@
             counter: 0,
             min: 0,
             max: 10,
-            section: {
-                rand: true,
-                randf: true,
-                randbound: true,
-                randboundlemire: true
-            }
+            section_rand: true,
+            section_randf: true,
+            section_randbound: true,
+            section_randboundlemire: true
         },
         watch: {
             key() {
-                this.renderCharts();
+                // Chart may not exist until sync complete
+                setTimeout(this.renderCharts.bind(this), 1);
             },
             counter() {
-                this.renderCharts();
+                setTimeout(this.renderCharts.bind(this), 1);
             },
             implementation() {
-                this.renderCharts();
+                setTimeout(this.renderCharts.bind(this), 1);
+            },
+            min() {
+                setTimeout(this.renderCharts.bind(this), 1);
+            },
+            max() {
+                setTimeout(this.renderCharts.bind(this), 1);
+            },
+            section_rand() {
+                setTimeout(this.renderCharts.bind(this), 1);
+            },
+            section_randf() {
+                setTimeout(this.renderCharts.bind(this), 1);
+            },
+            section_randbound() {
+                setTimeout(this.renderCharts.bind(this), 1);
+            },
+            section_randboundlemire() {
+                setTimeout(this.renderCharts.bind(this), 1);
             }
         },
         methods: {
@@ -118,7 +83,7 @@
                 let rng = this.implementation === IMPL_STATELESS ?
                     () => prngStateless.rand(ctr++, key) :
                     () => prngStateful.rand();
-                chart(ctx, rng);
+                app.chart(ctx, rng);
             },
             renderRandFChart(ctr, key) {
                 // May be called before registration
@@ -129,7 +94,7 @@
                 let rng = this.implementation === IMPL_STATELESS ?
                     () => prngStateless.randF(ctr++, key) :
                     () => prngStateful.randF();
-                chart(ctx, rng);
+                app.chart(ctx, rng);
             },
             renderRandBoundChart(ctr, key) {
                 // May be called before registration
@@ -140,17 +105,16 @@
                 let rng = this.implementation === IMPL_STATELESS ?
                     () => prngStateless.randBound(ctr++, key, this.min, this.max) :
                     () => prngStateful.randBound(this.min, this.max);
-                chart(ctx, rng);
+                app.chart(ctx, rng);
             },
             renderRandBoundLemireChart() {
                 // May be called before registration, method only available with stateful version
                 if (!this.$refs["chart-randboundlemire"] ||
                     this.implementation === IMPL_STATELESS) {
-                    console.log(`State: ${this.implementation === IMPL_STATELESS}`);
                     return;
                 }
                 let ctx = this.$refs["chart-randboundlemire"].getContext("2d");
-                chart(ctx, () => prngStateful.randBoundLemire(this.min, this.max));
+                app.chart(ctx, () => prngStateful.randBoundLemire(this.min, this.max));
             }
         }
     });
@@ -158,4 +122,4 @@
     // Randomly choose one of the 1000 keys
     app.vue.key = keys[Math.round(Math.random() * (keys.length - 1))];
 
-})(window.app = {});
+})(window.app = window.app || {});
---
+++ playground/assets/js/chart.js
@@ -0,0 +1,56 @@
+"use strict";
+
+(app => {
+    function yFormatter(label) {
+        if (label < 1000) {
+            return label;
+        }
+
+        if (label < 1000000) {
+            label / 1000 + "K"
+        }
+
+        return label < 1000000000 ?
+            label / 1000000 + "M" :
+            label / 1000000000 + "B";
+    };
+
+    app.chart = (ctx, rng) => {
+
+        const total = 100;
+        let data = new Array(total);
+        let labels = new Array(total);
+        for (let i = 0; i < total; ++i) {
+            data[i] = rng();
+            labels[i] = i;
+        }
+
+        new Chart(ctx, {
+            type: "line",
+            data: {
+                labels: labels,
+                datasets: [
+                    {
+                        data: data,
+                        fill: false,
+                        borderColor: "#36e"
+                    }
+                ]
+            },
+            options: {
+                legend: {
+                    display: false
+                },
+                responsive: true,
+                scales: {
+                    yAxes: [
+                        {
+                            ticks: { callback: yFormatter }
+                        }
+                    ]
+                }
+            }
+        });
+    };
+
+})(window.app = window.app || {});
