0ca17d9955f94adec3d96837fc9b002607253b32
Chad Hartman <cmhartman+github@gmail.com>
2020-10-06 13:17:05 -0700

    Added collapsable sections

--- playground/assets/js/app.js
+++ playground/assets/js/app.js
@@ -9,19 +9,14 @@
     let keys = await fetch(`assets/json/keys.${keySubsetIndex}.json`)
         .then(response => response.json());
 
-    let prngStateless = await fetch("assets/wasm/squaresrng.wasm")
+    let prngStateless = await fetch(`assets/wasm/squaresrng.wasm?cb=${Date.now()}`)
         .then(response => response.arrayBuffer())
         .then(buffer => WebAssembly.instantiate(buffer))
         .then(wasm => wasm.instance.exports);
 
-    let prngStateful = await fetch("assets/wasm/squaresrngs.wasm")
+    let prngStateful = await fetch(`assets/wasm/squaresrngs.wasm?cb=${Date.now()}`)
         .then(response => response.arrayBuffer())
-        .then(buffer => WebAssembly.instantiate(buffer, {
-            "state": {
-                key: new WebAssembly.Global({ value: "i64", mutable: true }, BigInt(0)),
-                ctr: new WebAssembly.Global({ value: "i64", mutable: true }, BigInt(0))
-            }
-        }))
+        .then(buffer => WebAssembly.instantiate(buffer))
         .then(wasm => wasm.instance.exports);
 
     function yFormatter(label) {
@@ -84,95 +79,79 @@
             key: "",
             counter: 0,
             min: 0,
-            max: 10
+            max: 10,
+            section: {
+                rand: true,
+                randf: true,
+                randbound: true,
+                randboundlemire: true
+            }
         },
         watch: {
-            key: this.renderCharts,
-            counter: this.renderCharts
+            key() {
+                this.renderCharts();
+            },
+            counter() {
+                this.renderCharts();
+            },
+            implementation() {
+                this.renderCharts();
+            }
         },
         methods: {
             renderCharts() {
+                let ctr = BigInt(this.counter);
+                let key = BigInt(this.key);
+                prngStateful.setKey(key);
+                prngStateful.setCtr(ctr);
+                this.renderRandChart(ctr, key);
+                this.renderRandFChart(ctr, key);
+                this.renderRandBoundChart(ctr, key);
+                this.renderRandBoundLemireChart();
+            },
+            renderRandChart(ctr, key) {
+                // May be called before registration
                 if (!this.$refs["chart-rand"]) {
                     return;
                 }
-                // May be called before registration
                 let ctx = this.$refs["chart-rand"].getContext("2d");
                 let rng = this.implementation === IMPL_STATELESS ?
                     () => prngStateless.rand(ctr++, key) :
                     () => prngStateful.rand();
                 chart(ctx, rng);
+            },
+            renderRandFChart(ctr, key) {
+                // May be called before registration
+                if (!this.$refs["chart-randf"]) {
+                    return;
+                }
+                let ctx = this.$refs["chart-randf"].getContext("2d");
+                let rng = this.implementation === IMPL_STATELESS ?
+                    () => prngStateless.randF(ctr++, key) :
+                    () => prngStateful.randF();
+                chart(ctx, rng);
+            },
+            renderRandBoundChart(ctr, key) {
+                // May be called before registration
+                if (!this.$refs["chart-randbound"]) {
+                    return;
+                }
+                let ctx = this.$refs["chart-randbound"].getContext("2d");
+                let rng = this.implementation === IMPL_STATELESS ?
+                    () => prngStateless.randBound(ctr++, key, this.min, this.max) :
+                    () => prngStateful.randBound(this.min, this.max);
+                chart(ctx, rng);
+            },
+            renderRandBoundLemireChart() {
+                // May be called before registration, method only available with stateful version
+                if (!this.$refs["chart-randboundlemire"] ||
+                    this.implementation === IMPL_STATELESS) {
+                    console.log(`State: ${this.implementation === IMPL_STATELESS}`);
+                    return;
+                }
+                let ctx = this.$refs["chart-randboundlemire"].getContext("2d");
+                chart(ctx, () => prngStateful.randBoundLemire(this.min, this.max));
             }
-            // ,
-            // rand() {
-            //     let ctr = BigInt(this.counter);
-            //     let key = BigInt(this.key);
-            //     prngStateful.setCtr(ctr);
-            //     prngStateful.setKey(key);
-
-            //     let rng = this.implementation === IMPL_STATELESS ?
-            //         () => prngStateless.rand(ctr++, key) >>> 0 :
-            //         () => prngStateful.rand() >>> 0;
-            //     let result = rng();
-
-            //     if (this.$refs["chart-rand"]) {
-            //         // May be called before registration
-            //         let ctx = this.$refs["chart-rand"].getContext("2d");
-            //         chart(ctx, rng);
-            //     }
-
-            //     return result;
-            // },
-            // randF() {
-            //     let ctr = BigInt(this.counter);
-            //     let key = BigInt(this.key);
-            //     prngStateful.setCtr(ctr);
-            //     prngStateful.setKey(key);
-
-            //     let rng = this.implementation === IMPL_STATELESS ?
-            //         () => prngStateless.randF(ctr++, key) :
-            //         () => prngStateful.randF();
-            //     let result = rng();
-
-            //     if (this.$refs["chart-randf"]) {
-            //         // May be called before registration
-            //         let ctx = this.$refs["chart-randf"].getContext("2d");
-            //         chart(ctx, rng);
-            //     }
-
-            //     return result;
-            // },
-            // randBound() {
-            //     let ctr = BigInt(this.counter);
-            //     let key = BigInt(this.key);
-            //     prngStateful.setCtr(ctr);
-            //     prngStateful.setKey(key);
-
-            //     if (this.max - this.min < 0) {
-            //         return "Invalid range; max must be >= min";
-            //     }
-
-            //     let rng = this.implementation === IMPL_STATELESS ?
-            //         () => prngStateless.randBound(ctr++, key, this.min, this.max) :
-            //         () => prngStateful.randBound(this.min, this.max);
-            //     let result = rng();
-
-            //     if (this.$refs["chart-randbound"]) {
-            //         // May be called before registration
-            //         let ctx = this.$refs["chart-randbound"].getContext("2d");
-            //         chart(ctx, rng);
-            //     }
-
-            //     return result;
-            // },
-            // randBoundLemire() {
-            //     // No stateless implementation available
-            //     prngStateful.setCtr(BigInt(this.counter));
-            //     prngStateful.setKey(BigInt(this.key));
-            //     return prngStateful.randBound(this.min, this.max);
-            // },
-            // getCtr() {
-            //     return prngStateful.ctr();
-            // }
         }
     });
 
